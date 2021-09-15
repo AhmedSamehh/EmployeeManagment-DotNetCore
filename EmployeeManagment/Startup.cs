3a3cb796a4a847843cf0616a72d40669e2050a07
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2021-09-15 19:46:46 +0200

    added entity framework

--- EmployeeManagment/Startup.cs
+++ EmployeeManagment/Startup.cs
@@ -6,6 +6,8 @@ using EmployeeManagment.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,8 +17,11 @@ namespace EmployeeManagment
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private static readonly IConfiguration _conf;
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_conf.GetConnectionString("EmployeeDBConnection")));
             services.AddMvc(option => option.EnableEndpointRouting = false).AddXmlSerializerFormatters();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
